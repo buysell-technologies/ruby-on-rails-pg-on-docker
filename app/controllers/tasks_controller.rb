8be864f4b19195741df7d6e5b0b5d4fc1992d8ee
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-12 13:06:09 +0900

    Step7_1 登録画面を作成 登録機能追加 登録した内容を一覧画面に表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,22 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    task = params[:task].permit(:title, :description)
+    Task.create(task)
+    redirect_to root_path, notice: "タスク「#{task.title}」を登録しました"
   end
 
   def edit
   end
+
 end
