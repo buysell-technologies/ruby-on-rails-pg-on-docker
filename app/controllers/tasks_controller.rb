21800e5c438e137eb13f4d4b98163a5d09db2679
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-12 16:51:04 +0900

    Step7_2 タスクの編集機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,10 +14,17 @@ class TasksController < ApplicationController
   def create
     task = params[:task].permit(:title, :description)
     Task.create(task)
-    redirect_to root_path, notice: "タスク「#{task.title}」を登録しました"
+    redirect_to root_path, notice: "タスク「#{task.title}」を登録しました。"
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(params[:task].permit(:title, :description))
+    redirect_to root_path, notice: "タスク「#{task.title}」を更新しました。"
   end
 
 end
