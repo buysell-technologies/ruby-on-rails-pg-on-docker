dd9fada5c298587221d6ceb686a502c4ba7ea01b
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-13 15:44:41 +0900

    Step9 日本語設定

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,12 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators.template_engine = :slim
+
+    #i18で日本語化
+    config.i18n.default_locale = :ja
+
+    # 日本語化するファイルのパスを指定
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
