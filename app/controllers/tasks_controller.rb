e7b064a020c2bea98bac48d0f49571128cc64655
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-18 11:24:26 +0900

    終了期限を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,7 +35,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :deadline_date)
   end
 
 end
