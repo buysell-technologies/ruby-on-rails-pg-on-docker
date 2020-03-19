03844f690a7494ab1c7c36621b1286e60ead89fc
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-19 17:31:38 +0900

    RSpec letで記述 テストデータに期限とステータスを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :task do
     title { '掃除' }
     description { '家の掃除をする' }
+    deadline_date { '2020-03-03' }
+    status { '完了' }
     created_at { Time.now }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,46 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it"タイトルと内容がある場合、有効" do
-    @task = FactoryBot.build(:task)
-    expect(@task).to be_valid
-  end
-
-  it"タイトルがない場合、無効" do
-    task = Task.new(
-      title: nil,
-      description: "キッチン"
-    )
-    task.valid?
-    expect(task).to_not be_valid
-  end
-
-  describe 'タイトルの文字数' do
-    context 'タイトルが30文字以内場合' do
-      it"有効" do
-        @task = FactoryBot.build(:task)
-        expect(@task).to be_valid
-      end
-    end
-
-    context 'タイトルが30文字以上の場合' do
-      it "無効" do
-        task = Task.new(
-          title: "s" * 31,
-          description: "キッチン"
-        )
-        expect(task).to_not be_valid
-      end
-    end
-  end
-
-  it"内容がない場合、無効" do
-    task = Task.new(
-      title: "家事",
-      description: nil
-    )
-    task.valid?
-    expect(task).to_not be_valid
-  end
+  let(:task){ FactoryBot.build(:task) }
   
+  describe 'バリデーション' do
+    it 'タイトルと内容がある場合、有効' do
+      expect(task).to be_valid
+    end
+  
+    it 'タイトルがない場合、無効' do
+      task.title = ''
+      expect(task).to_not be_valid
+    end
+  
+    describe 'タイトルの文字数' do
+      context 'タイトルが30文字以内場合' do
+        it '有効' do
+          expect(task).to be_valid
+        end
+      end
+  
+      context 'タイトルが30文字以上の場合' do
+        it '無効' do
+          task.title = "s" * 31
+          expect(task).to_not be_valid
+        end
+      end
+    end
+  
+    it '内容がない場合、無効' do
+      task.description = ''
+      expect(task).to_not be_valid
+    end
+  end
 end
