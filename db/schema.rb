4b6c4ecfe829c39a07f2d21eae9c477721088267
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-24 19:34:33 +0900

    ユーザテーブルとタスクテーブルの紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_094340) do
+ActiveRecord::Schema.define(version: 2020_03_24_102511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_03_24_094340) do
     t.date "deadline_date"
     t.integer "status", limit: 2
     t.boolean "important", default: false, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
