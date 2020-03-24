572c5b126f520bd70043e42980bfe620defa7424
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-24 19:38:31 +0900

    1対多の関係を定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
     validates :deadline_date, presence: true
     enum status: {未着手: 0, 作業中: 1, 完了: 2}
 
+    belongs_to :user
+
     ransacker :keywords do |tasks|
         Arel::Nodes::InfixOperation.new('||', tasks.table[:title], tasks.table[:description])
       end
