639acdcf29511392e9028c7d250a314748a05c4e
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-24 19:26:30 +0900

    password_digest修正

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_094340) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
