4a40ff51a72115e8998764207c91f974b2417bc8
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-24 18:35:39 +0900

    期限のバリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     validates :title, length: { minimum: 1, maximum: 30 }
     validates :description, presence: true
+    validates :deadline_date, presence: true
     enum status: {未着手: 0, 作業中: 1, 完了: 2}
 
     ransacker :keywords do |tasks|
