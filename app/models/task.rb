b1a49a31feb8b0cf4cfaf77286bcabaccdb822a7
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-24 17:21:11 +0900

    誤字修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
     validates :title, length: { minimum: 1, maximum: 30 }
     validates :description, presence: true
-    enum status: {todo: 0, doing: 1, done: 2}
+    enum status: {未着手: 0, 作業中: 1, 完了: 2}
 
     ransacker :keywords do |tasks|
         Arel::Nodes::InfixOperation.new('||', tasks.table[:title], tasks.table[:description])
