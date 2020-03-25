df5a440c722e8f6a5c2fa2e8c59de609fbc78292
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-25 14:37:24 +0900

    ログインチェック機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :login_required
+
+    private
+    def login_required
+        redirect_to login_path unless current_user
+    end
 end
