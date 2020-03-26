72beccb6ead34ea5a68c417f26a34809639d2f2a
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-03-26 16:01:33 +0900

    ユーザー登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness;: true
+    validates :password, length: { minimum: 6 }, presence: true
+
     has_many :tasks
 end
