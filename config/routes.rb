0574d5e1dae3f55d96ac856fcf5528d09f99740a
Shun Tomizawa <s.tomizawa@buysell-technologies.com>
2020-04-03 12:53:08 +0900

    create task routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #トップページはタスク一覧
+  root to: 'tasks#index'
+  #index のみ排除
+  resources :tasks, except: [:index]
 end
