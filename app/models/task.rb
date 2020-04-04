f533f43ef00ea38a30c15132e199798bcf8758cf
Shun Tomizawa <s.tomizawa@buysell-technologies.com>
2020-04-04 18:32:04 +0900

    Taskのname,content,status,priorityに バリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+    # 空文字とStringが255までだからそれ以上は除去
+    validates :name, presence: true, length: {maximum: 255}
+    # 空文字を許し最大400文字としておく
+    validates :content, length: {maximum: 400}
+    # statusは0~2の整数
+    validates :status, presence: true, numericality: {only_integer: true, less_than: 3}
+    # priorityは0~4の整数
+    validates :priority,  presence: true, numericality: {only_integer: true, less_than: 5}
 end
