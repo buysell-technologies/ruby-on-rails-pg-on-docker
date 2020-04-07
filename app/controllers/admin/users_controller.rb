df9a32d87783d7ba5099fdd2f01817e1600556ab
Shintaro9029 <s.sakai@buysell-technologies.com>
2020-04-07 12:06:57 +0900

    ユーザーテーブルにAdminカラムを追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -51,6 +51,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def require_admin
-    redirect_to root_path unless current_user
+    redirect_to root_path unless current_user.admin?
   end
 end
