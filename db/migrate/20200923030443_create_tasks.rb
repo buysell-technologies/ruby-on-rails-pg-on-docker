881d5dc11765eb3737f262bc0bb3e6afcb27933d
dshibuya <d.shibuya@buysell-technologies.com>
2020-09-23 12:06:40 +0900

    add Task model

---
+++ db/migrate/20200923030443_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :text
+      t.string :deadline
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
