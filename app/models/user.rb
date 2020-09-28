160bdd05edd96b3b7fb19c4759580a810cd8d7dc
dshibuya <d.shibuya@buysell-technologies.com>
2020-09-28 16:19:51 +0900

    update user,rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    validates :name, presence :true
+    validates :name, presence: true, length: {maximum:15}
     validates :password_digest, length: {minimum:8, maximum:20}
 end
