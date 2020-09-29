09eccee741d7d2ef5f043bf7ffeea968a0b8fdef
dshibuya <d.shibuya@buysell-technologies.com>
2020-09-29 15:34:28 +0900

    Basis認証の改良

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
-   before_action :basic
-
-   private
-   def basic
-      authenticate_or_request_with_http_basic do |name,password|
-         # name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
-         name == '1' && password == '2'
-      end
-   end
+   http_basic_authenticate_with :name => ENV['BASIC_AUTH_NAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 end
 
