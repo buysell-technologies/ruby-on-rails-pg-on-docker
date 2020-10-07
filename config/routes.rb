ae98ddc00bc05709a90164d01089ab19e3464f4c
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2020-10-07 10:07:53 +0900

    不要なファイルを削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  get 'tasks/search_title' => 'tasks#search_title'
-  get 'tasks/search_status' => 'tasks#search_status'
   resources :tasks
 
 end
